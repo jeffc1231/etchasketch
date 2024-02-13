d88cba366daf036a53ddf3d8ec1f5fa77a36b513
jeffc1231 <118142463+jeffc1231@users.noreply.github.com>
2024-02-13 15:58:28 -0500

    everything is finished

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function makeGrid(size, container) {
-	// Eliminar los hijos 
+	
 	container.innerHTML = '';
 
-	// Calcular el tamaño de cada celda para ajustarse a 960px
+	
 	const cellSize = 1020 / size;
 
 	for (let i = 0; i < size; i++) {
 		let column = document.createElement("div")
-		column.style.width = `${cellSize}px`; // Establecer el ancho de la columna
+		column.style.width = `${cellSize}px`; 
 
 		for (let j = 1; j <= size; j++) {
 			let row = document.createElement("div")
 			row.style.border = "1px solid rgb(20 20 20)";
-			row.style.width = "100%"; // Ajustar el ancho de la fila al 100%
-			row.style.height = `${cellSize}px`; // Ajustar la altura de la fila
+			row.style.width = "100%";
+			row.style.height = `${cellSize}px`;
 			column.appendChild(row);
 		}
 
@@ -39,7 +39,7 @@ number.addEventListener("change", (e) => {
 function newGrid() {
 
 	if (inputValue >= 1 && inputValue <= 100) {
-		//Necesito eliminar la cuadrilla anterior y crear una nueva con el valor ingresado en el inputValue
+		
 		makeGrid(inputValue, container)
 	} else {
 		alert("Debe de ingresar un numero entre 1 y 100")
